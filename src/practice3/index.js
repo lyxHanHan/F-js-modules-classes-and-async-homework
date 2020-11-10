f42529f97825c1882cf4f6b278b75514ac23769e
lyxHanHan <yixuan.li1@thoughtworks.com>
2020-11-10 23:20:32 +0800

    complete practice1、2、3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,10 @@
-export const getPoetry
+export const getPoetry = () => {
+  return fetch("https://v1.jinrishici.com/all.json", { method: "get" })
+    .then((res) => res.json())
+    .then((data) => {
+      return [data.origin, data.author, data.content];
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
