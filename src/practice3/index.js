41e9a85a298e7781a9cb0f1409f3504682fdcab9
lyxHanHan <yixuan.li1@thoughtworks.com>
2020-11-10 23:28:46 +0800

    complete practice3 by async

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,10 +1,24 @@
-export const getPoetry = () => {
-  return fetch("https://v1.jinrishici.com/all.json", { method: "get" })
-    .then((res) => res.json())
-    .then((data) => {
-      return [data.origin, data.author, data.content];
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+// export const getPoetry = () => {
+//   return fetch("https://v1.jinrishici.com/all.json", { method: "get" })
+//     .then((res) => res.json())
+//     .then((data) => {
+//       return [data.origin, data.author, data.content];
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
+
+export const getPoetry = async () => {
+  const res = await fetch("https://v1.jinrishici.com/all.json", {
+    method: "get",
+  });
+
+  const data = await res.json();
+
+  if (data != null) {
+    return [data.origin, data.author, data.content];
+  }
+
+  return new Error("err");
 };
